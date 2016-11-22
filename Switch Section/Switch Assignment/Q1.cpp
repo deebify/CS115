947e2895f033a4cd0a720414f3728d7f21997710
deebify <yassine.deeb@msn.com>
2016-11-22 18:56:01 +0200

    Challenge and Assignment

---
+++ Switch Section/Switch Assignment/Q1.cpp
@@ -0,0 +1,26 @@
+/**
+
+This is a Challenging Question  (Not an Assignment): 
+
+   Write a program using Switch that simply solve roots of a the quadratic equation.
+
+   What's the Quadratic Equation? 
+        (a)x^2 + (b)y + (c) = 0
+    
+    where a, b, c are real numbers. 
+
+    To get roots for this equation there's cases (2 cases).
+    Using Switch-cases write the each case.
+
+    Hints:
+        - you can use 2 nested switch :P :P 
+
+    Input  = a, b, c (3 variables)
+    Output = 2 roots (2 variables)
+
+
+Test your code:
+ Input  = 4 -2 -10
+ Output =  1.85 -1.35 
+
+**/
---
+++ Switch Section/Switch Assignment/Q2.cpp
@@ -0,0 +1,26 @@
+
+/**
+An Assignment 
+    (If you Solve the Challenge, its Optional Recommended Assignment):
+
+    Using Switch and If: 
+    Write the Following Program that:
+        Accecpt your Birthday and the Current Date
+        then Output your Age. 
+    
+    Input As the following:
+        birth day , birth month, birth year
+        current day, current month , current year
+    
+    Notes: 
+        -- make sure user input valid number for both month and day for current 
+            date and birthdate
+            -> month must be  1 < month > 12
+            -> day must be  1 < day > 31
+
+        -- make sure that month feb. sometimes come with 28 or 29 days 
+            based on the current year.
+
+        -- output must be like the following:
+            your age: "x years, y months, z days"   
+**/
