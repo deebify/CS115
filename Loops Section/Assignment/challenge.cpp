1bd85151d9aa8808a8b47c845f67e989229daf21
deebify <yassine.deeb@msn.com>
2016-12-05 02:15:33 +0200

    Challanges and Assignment Added

---
+++ Loops Section/Assignment/challenge.cpp
@@ -0,0 +1,34 @@
+/**
+    Challange #2: 
+
+    a very special challange using loop control structure
+    such as (while/do-while/for) .. 
+
+
+    App #1) 
+    
+    -- Show me the Reverse of given Number  
+    For Example: 
+        given the number such 1992 as input
+        show me the output as  2991 
+        
+    test_cases: 
+        input  =  12345
+        output = 54321
+
+    ---------------------------------------------------------------------------
+
+    App #2) 
+
+    -- Given number x just tell me is that prime number or not. 
+    For Example: 
+        given the number such 5 as input 
+        output will be "PRIME"
+    
+    test_cases: 
+        input = 4
+        output = "NOT PRIME"
+
+        input = 2
+        output = "PRIME"
+**/
